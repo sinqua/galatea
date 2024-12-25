e29dc8d19cd43393d922c787c4d341a41096a3bc
규화 신 <quashine@icloud.com>
2024-12-25 18:33:07 +0900

    주석 처리된 코드 제거 및 AudioContext 관련 코드 정리

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,28 @@ import InputField from "./input-field";
 export default function Home() {
 
   useEffect(() => {
-    window.SpeechBlendWEBGL = {};
+    // window.SpeechBlendWEBGL = {};
 
-    window.AudioContext = (function(){
-      const ACConsructor = window.AudioContext || window.webkitAudioContext;
+    // window.AudioContext = (function(){
+    //   const ACConsructor = window.AudioContext || window.webkitAudioContext;
 
-      return function(){
-        const ac = new ACConsructor();
-        window.SpeechBlendWEBGL.ac = ac;
-        window.SpeechBlendWEBGL.a = ac.createAnalyser();
-        window.SpeechBlendWEBGL.a.smoothingTimeConstant = 0;
-        window.SpeechBlendWEBGL.fa = new Uint8Array(window.SpeechBlendWEBGL.a.frequencyBinCount); 
-        window.SpeechBlendWEBGL.la = new Uint8Array(window.SpeechBlendWEBGL.a.fftSize); 
-        window.SpeechBlendWEBGL.a.connect(ac.destination); 
+    //   return function(){
+    //     const ac = new ACConsructor();
+    //     window.SpeechBlendWEBGL.ac = ac;
+    //     window.SpeechBlendWEBGL.a = ac.createAnalyser();
+    //     window.SpeechBlendWEBGL.a.smoothingTimeConstant = 0;
+    //     window.SpeechBlendWEBGL.fa = new Uint8Array(window.SpeechBlendWEBGL.a.frequencyBinCount); 
+    //     window.SpeechBlendWEBGL.la = new Uint8Array(window.SpeechBlendWEBGL.a.fftSize); 
+    //     window.SpeechBlendWEBGL.a.connect(ac.destination); 
         
-        ac.actualDestination = ac.destination;
-        Object.defineProperty(ac, 'destination', { 
-          value: window.SpeechBlendWEBGL.a,
-          writable: false
-        });   
-        return ac; 
-      }
-    })();
+    //     ac.actualDestination = ac.destination;
+    //     Object.defineProperty(ac, 'destination', { 
+    //       value: window.SpeechBlendWEBGL.a,
+    //       writable: false
+    //     });   
+    //     return ac; 
+    //   }
+    // })();
   }, []);
 
 
