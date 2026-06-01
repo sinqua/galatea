21c3cd388302af3e54927461cbbe64a77b78b936
신규화 <sinq0216@gmail.com>
2026-06-01 12:05:39 +0900

    flask_cors 패키지 추가

--- api/index.py
+++ api/index.py
@@ -2,28 +2,19 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from flask import Flask, request, make_response
+from flask import Flask, request
+from flask_cors import CORS
 import llm
 
 app = Flask(__name__)
-ALLOWED_ORIGIN = os.getenv('ALLOWED_ORIGIN', '*')
+CORS(app, origins=os.getenv('ALLOWED_ORIGIN', '*'))
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/textonly', methods=['POST', 'OPTIONS'], strict_slashes=False)
+@app.route('/textonly', methods=['POST'], strict_slashes=False)
 def hello_text2():
-    if request.method == 'OPTIONS':
-        res = make_response()
-        res.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGIN
-        res.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        res.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return res
-
     text = request.form['text']
     message = llm.chat_ai(text)
-
-    res = make_response(message)
-    res.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGIN
-    return res
+    return message
