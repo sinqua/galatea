7d5dc36e7b1968250d73af3184e6d3dd492a3774
신규화 <sinq0216@gmail.com>
2026-06-04 11:37:52 +0900

    google tts -> aws polly

--- api/voice.py
+++ api/voice.py
@@ -1,27 +1,14 @@
-"""Synthesizes speech from the input string of text or ssml.
-Make sure to be working in a virtual environment.
+import boto3
 
-Note: ssml must be well-formed according to:
-    https://www.w3.org/TR/speech-synthesis/
-"""
-from google.cloud import texttospeech
-
-def speech(userText) -> bytes:
-    """Convert text to MP3 audio bytes using Google Cloud TTS."""
-    client = texttospeech.TextToSpeechClient()
-
-    synthesis_input = texttospeech.SynthesisInput(text=userText)
-
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
-    )
-
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
+def speech(userText: str) -> bytes:
+    """Convert text to MP3 audio bytes using AWS Polly."""
+    client = boto3.client('polly', region_name='us-east-1')
 
     response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config
+        Text=userText,
+        OutputFormat='mp3',
+        VoiceId='Joanna',   # 영어 여성 음성. 한국어면 'Seoyeon' 사용
+        Engine='neural',
     )
 
-    return response.audio_content
+    return response['AudioStream'].read()
