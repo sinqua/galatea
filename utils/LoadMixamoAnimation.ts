c31b952e81f8b91f38de100e69aa9969b88a7e1d
신규화 <sinq0216@gmail.com>
2026-06-04 11:03:09 +0900

    FBXLoader 임포트 경로 수정

--- utils/LoadMixamoAnimation.ts
+++ utils/LoadMixamoAnimation.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 import { mixamoVRMRigMap } from "./MixamoVrmRigMap";
 
 export function LoadMixamoAnimation(url: any, vrm: any) {
