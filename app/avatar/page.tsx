a49147d946e840b34ef4fb0acb5271aba8079f6c
신규화 <sinq0216@gmail.com>
2026-06-04 15:30:26 +0900

    일시적 T-pose 보이는 현상 수정

--- app/avatar/page.tsx
+++ app/avatar/page.tsx
@@ -48,7 +48,9 @@ function VRMAvatar({ onReady, debugRef: externalDebugRef }: VRMAvatarProps) {
           mat.toneMapped = false;
         });
 
+        loaded.scene.traverse((obj) => { obj.frustumCulled = false; });
         loaded.scene.rotation.y = Math.PI;
+        loaded.scene.visible = false;
         scene.add(loaded.scene);
         vrmRef.current = loaded;
         setVrm(loaded);
@@ -98,6 +100,7 @@ function VRMAvatar({ onReady, debugRef: externalDebugRef }: VRMAvatarProps) {
 
     landingAction.loop = THREE.LoopOnce;
     landingAction.clampWhenFinished = true;
+    if (vrmRef.current) vrmRef.current.scene.visible = true;
     landingAction.play();
 
     const onFinished = (e: any) => {
